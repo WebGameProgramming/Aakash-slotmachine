1861429c401cc03549fdb14ddf66638ac1ec6674
aakash <oza.aakash@gmail.com>
2016-02-23 19:12:28 -0500

    adding slot machine image to the game scene

--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -2,7 +2,7 @@
 module scenes {
     export class SlotMachine extends objects.Scene {
         //PRIVATE INSTANCE VARIABLES ++++++++++++
-        private _slotMachineLabel:objects.Label;
+       private _backgroundImage: createjs.Bitmap
         
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
@@ -13,15 +13,11 @@ module scenes {
         
         // Start Method
         public start(): void {    
+            // add backgroundimage to the scene
             
-            // add the SLOT MACHINE Label to the MENU scene
-            this._slotMachineLabel = new objects.Label(
-                "GAME GOES HERE", 
-                "60px Consolas", 
-                "#000000", 
-                config.Screen.CENTER_X, 
-                config.Screen.CENTER_Y);
-            this.addChild(this._slotMachineLabel);          
+            this._backgroundImage= new createjs.Bitmap(assets.getResult("SlotMachine"));
+            this.addChild(this._backgroundImage);
+            
             
             // add this scene to the global stage container
             stage.addChild(this);

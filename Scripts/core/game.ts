e703f170ff98f299e20c8c7fd216fc6917126b64
aakash <oza.aakash@gmail.com>
2016-02-26 15:35:25 -0500

    minor update

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -22,7 +22,7 @@ var assetData:objects.Asset[] = [
     {id: "SlotMachine", src:"../../Assets/images/SlotMachine.png"},
     {id: "Bet100Button", src:"../../Assets/images/Bet100Button.png"},
     {id: "Bet150Button", src:"../../Assets/images/Bet150Button.png"},
-    {id: "Bet500BUtton", src:"../../Assets/images/Bet500BUtton.png"},
+    {id: "Bet500BUtton", src:"../../Assets/images/Bet500Button.png"},
     {id: "SpinButton", src:"../../Assets/images/SpinButton.png"},
 ];
 
